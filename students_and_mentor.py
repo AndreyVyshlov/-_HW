627b2662ab46e299321d9a5bcafa58b87dcf870f
Andrey Vyshlov <an.vyshlov@yandex.ru>
2022-09-01 14:09:40 +0300

    fixed bugs

--- students_and_mentor.py
+++ students_and_mentor.py
@@ -30,7 +30,6 @@ class Student:
                 self.midgrade = sum(grades_list) / len(grades_list)
             return self.midgrade
 
-
     def __lt__(self, other):
         if isinstance(other, Student):
             return self.midgrade < other.midgrade
@@ -43,8 +42,6 @@ class Student:
                 f'Средняя оценка за домашние задания: {self._midgrade()}\n'
                 f'Курсы в процессе изучения: {", ".join(self.courses_in_progress)}\n'
                 f'Завершенные курсы: {", ".join(self.finished_courses)}\n')
-
-
 
 
 class Mentor:
@@ -71,11 +68,17 @@ class Lecturer(Mentor):
             self.midgrade = sum(grades_list) / len(grades_list)
         return self.midgrade
 
+    def __lt__(self, other):
+        if isinstance(other, Lecturer):
+            return self.midgrade < other.midgrade
+        else:
+            return 'Ошибка!'
 
     def __str__(self):
         return(f'Имя: {self.name}\n'
               f'Фамилия: {self.surname}'
               f'Средняя оценка за лекции: {self._midgrade()}\n')
+
 
 class Reviewer(Mentor):
     def __init__(self, name, surname):
@@ -104,21 +107,21 @@ student1.courses_in_progress = ['Python', 'C++', 'C#']
 student1.finished_courses = ['Java', 'Math', 'SQL']
 
 student2 = Student('Shurik', 'Kuznetcov', 'Man')
-student1.courses_in_progress = ['Python']
-student1.finished_courses = ['Java', 'Math', 'SQL', 'C++', 'C#']
+student2.courses_in_progress = ['Python']
+student2.finished_courses = ['Java', 'Math', 'SQL', 'C++', 'C#']
 
 lecturer1 = Lecturer('Jack', 'Vorobey')
 lecturer1.courses = ['Python', 'SQL']
 
 lecturer2 = Lecturer('Tony', 'Stark')
-lecturer2.courses = ['Java', 'Math', 'C++', 'C#']
+lecturer2.courses = ['Java', 'Math', 'C++', 'Python']
 
 reviewer1 = Reviewer('Oleg', 'Tinkov')
-reviewer1.courses = ['SQL', 'Python', 'C++']
+reviewer1.courses_attached = ['SQL', 'Python', 'C++']
 
 reviewer1.rate_hw(student2,'SQL', 10)
-reviewer1.rate_hw(student2,'SQL', 4)
-reviewer1.rate_hw(student2,'C++', 5)
+reviewer1.rate_hw(student2, 'SQL', 4)
+reviewer1.rate_hw(student2,'Python', 5)
 reviewer1.rate_hw(student1,'Python', 7)
 reviewer1.rate_hw(student1,'SQL', 1)
 reviewer1.rate_hw(student1,'Python', 10)
@@ -126,10 +129,10 @@ reviewer1.rate_hw(student1,'Python', 10)
 reviewer2 = Reviewer('Andrey', 'Vyshlov')
 reviewer2.courses_attached = ['Java', 'C#']
 
-student1.lect_rate(lecturer1,'SQL', 7)
-student2.lect_rate(lecturer1,'SQL', 5)
-student1.lect_rate(lecturer2,'Math', 3)
-student2.lect_rate(lecturer2,'Math', 10)
+student1.lect_rate(lecturer1, 'Python', 7)
+student2.lect_rate(lecturer1, 'Python', 5)
+student1.lect_rate(lecturer2, 'Python', 3)
+student2.lect_rate(lecturer2, 'Python', 10)
 
 
 print(student1)
@@ -161,14 +164,15 @@ def student_midgrades(students, course):
 def lecturer_midgrades(lecturers, course):
     all_course_grades = []
     for lecturer in lecturers:
-        for grade in lecturer.grades.get(course):
+        for grade in lecturer.courses_grades.get(course):
             all_course_grades += [grade]
     mid_grade = sum(all_course_grades) / len(all_course_grades)
     print(f'Средняя оценка за курс {course}: {mid_grade}')
 
-
-student_midgrades(students_list,courses[1])
-lecturer_midgrades(students_list,courses[3])
+print(student1.grades)
+print(lecturer1.courses_grades)
+student_midgrades(students_list, courses[2])
+lecturer_midgrades(lecturers_list, courses[2])
 
 
 
